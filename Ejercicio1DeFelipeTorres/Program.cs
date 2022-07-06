a39f7d774c492d4c133880241d6750fa1380235c
Felipe Torres <felipetorres5752@gmail.com>
2022-07-05 22:17:57 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1DeFelipeTorres/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio1DeFelipeTorres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			double Kilometros;
+			int Distancia;
+			Console.WriteLine("¿A que Velosidad se Desplaza?");
+			int Velocidad = int.Parse(Console.ReadLine()) ;
+
+			Console.WriteLine("¿Cuanto Tiempo?");
+			int Tiempo = int.Parse(Console.ReadLine());
+
+			Distancia = Velocidad + Tiempo;
+			Kilometros = Distancia * 0.001;
+
+			Console.WriteLine($"Recorrio {Distancia} M/S .Es igual a {Kilometros} Km/S");
+
+			Console.ReadLine();
+		}
+	}
+}
